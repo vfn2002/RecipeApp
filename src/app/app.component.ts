ce5b7adeca9b82801f8ff891fd507bc8cb17efea
victornielsen <vfn@live.dk>
2018-04-23 20:08:51 +0200

    config changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,13 +42,13 @@ export class MyApp {
           this.rootPage = IntroPage;
           storage.set('intro_completed', true);
         }
+        statusBar.styleDefault();
+        splashScreen.hide();
       });
  
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       
-      statusBar.styleDefault();
-      splashScreen.hide();
     });
   }
 }
